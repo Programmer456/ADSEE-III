1d7eecad1c0f14cddd23ae879216254bdae4c645
marknibbelke <m.h.nibbelke@student.tudelft.nl>
2024-03-23 13:48:02 +0100

    test

--- Reference_Params_temp.py
+++ Reference_Params_temp.py
@@ -90,33 +90,6 @@ class Reference_Params:
 
 
 
-length_total = 35.53 # meters ( maybe includes tail)
-height_total = 8.51 # meters
-length_fuselage = 32.5 #meters
-height_fuselage = 3.3 #meters
-b = 28.08 #meters
-mac = 0 #TO BE FOUND ADSEE II SLIDES FORMULAS
-mac_h =  0
-chord_root = 5.28 #meters
-chord_tip = 1.26 #meters
-Ar = 8.4 #meters
-b_h = 10.04 #meters
-wheelbase = 14.01 #meters
-wheeltrack = 5.04 #meters
-lenght_seatingarea = 18.8 #meters
-lenght_cabin = 21.19 #meters excluding flight deck
-Sh = 17.76 #m^2
-S = 93.5 #m^2
-'weight [c]'
-OEW = 24747 #Kg
-MZFW = 36740 #Kg
-MRW = 46040 #Kg
-MTOW = 45810 #Kg
-MLW = 39915 #Kg
-MPW = 11993 #Kg
-V_app_max = 241*3.6 #m/s
-Vh_V = 1 # T- Tail vh/v
-
 
 
 
